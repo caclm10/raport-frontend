d841e5b0a3058fd8db54c48138b481aaa125e17f
caclm10 <lewinxander@gmail.com>
2022-07-16 19:05:19 +0700

    add some changes

--- src/hooks/use-auth.js
+++ src/hooks/use-auth.js
@@ -2,6 +2,7 @@ import { useCallback, useContext } from "react"
 import { useNavigate } from 'react-router-dom'
 import authContext from "../context/auth/auth-context"
 import ajax from "../lib/ajax"
+import { useDataStore } from "../stores/data-store"
 import { toggleFullLoading } from "../stores/ui-store"
 
 const useAuth = () => {
@@ -11,11 +12,13 @@ const useAuth = () => {
 export const useLogout = () => {
     const navigate = useNavigate()
     const auth = useAuth()
+    const resetDataStore = useDataStore(state => state.reset)
 
     const logout = useCallback(async () => {
         toggleFullLoading('Logging out...')
         await ajax('/api/logout', 'POST')
         auth.logout()
+        resetDataStore()
         navigate('/auth/login')
     }, [])
 
--- src/stores/data-store.js
+++ src/stores/data-store.js
@@ -1,16 +1,24 @@
 import create from 'zustand'
 import ajax from '../lib/ajax'
 
-export const useDataStore = create((set, get) => ({
+const initialState = {
     students: [],
     studentsPaginator: null,
     shouldFetchStudents: true,
     isFetching: false,
+}
+
+export const useDataStore = create((set, get) => ({
+    ...initialState,
     fetchStudents: async (url = '/api/siswa') => {
         set({ isFetching: true })
         const result = await ajax(url)
         const { data, ...paginator } = result.data
 
-        set({ students: data, studentsPaginator: paginator, isFetching: false })
+
+        set({ students: data, studentsPaginator: paginator, isFetching: false, shouldFetchStudents: false })
+    },
+    reset: () => {
+        set(initialState)
     }
 }))
